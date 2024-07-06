31e8f3946a82e2b3ce6b8da8befec34213d1331a
pasturryx <felipeneug@gmail.com>
2024-07-05 23:27:27 -0400

    bring me to + travel pk

--- data-otservbr-global/npc/captain_bluebear.lua
+++ data-otservbr-global/npc/captain_bluebear.lua
@@ -1,76 +1,84 @@
+-- NPC Configuration
 local internalNpcName = "Captain Bluebear"
 local npcType = Game.createNpcType(internalNpcName)
-local npcConfig = {}
-
-npcConfig.name = internalNpcName
-npcConfig.description = internalNpcName
-
-npcConfig.health = 100
-npcConfig.maxHealth = npcConfig.health
-npcConfig.walkInterval = 2000
-npcConfig.walkRadius = 2
-
-npcConfig.outfit = {
-	lookType = 129,
-	lookHead = 19,
-	lookBody = 69,
-	lookLegs = 125,
-	lookFeet = 50,
-	lookAddons = 0,
+local npcConfig = {
+    name = internalNpcName,
+    description = internalNpcName,
+    health = 100,
+    maxHealth = 100,
+    walkInterval = 2000,
+    walkRadius = 2,
+    outfit = {
+        lookType = 129,
+        lookHead = 19,
+        lookBody = 69,
+        lookLegs = 125,
+        lookFeet = 50,
+        lookAddons = 0
+    },
+    flags = {
+        floorchange = false
+    },
+    voices = {
+        interval = 15000,
+        chance = 50,
+        { text = "Passages to Carlin, Ab'Dendriel, Edron, Venore, Port Hope, Liberty Bay, Yalahar, Roshamuul, Krailos, Oramond and Svargrond." }
+    }
 }
 
-npcConfig.flags = {
-	floorchange = false,
-}
-
-npcConfig.voices = {
-	interval = 15000,
-	chance = 50,
-	{ text = "Passages to Carlin, Ab'Dendriel, Edron, Venore, Port Hope, Liberty Bay, Yalahar, Roshamuul, Krailos, Oramond and Svargrond." },
-}
-
+-- Keyword Handling
 local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
-
 npcType.onThink = function(npc, interval)
-	npcHandler:onThink(npc, interval)
+    npcHandler:onThink(npc, interval)
 end
-
 npcType.onAppear = function(npc, creature)
-	npcHandler:onAppear(npc, creature)
+    npcHandler:onAppear(npc, creature)
 end
-
 npcType.onDisappear = function(npc, creature)
-	npcHandler:onDisappear(npc, creature)
+    npcHandler:onDisappear(npc, creature)
 end
-
 npcType.onMove = function(npc, creature, fromPosition, toPosition)
-	npcHandler:onMove(npc, creature, fromPosition, toPosition)
+    npcHandler:onMove(npc, creature, fromPosition, toPosition)
 end
-
 npcType.onSay = function(npc, creature, type, message)
-	npcHandler:onSay(npc, creature, type, message)
+    npcHandler:onSay(npc, creature, type, message)
+end
+npcType.onCloseChannel = function(npc, creature)
+    npcHandler:onCloseChannel(npc, creature)
 end
 
-npcType.onCloseChannel = function(npc, creature)
-	npcHandler:onCloseChannel(npc, creature)
-end
-local function addInstantTravel(keywordHandler, destination, cost, position)
+-- Travel Options
+local function addInstantTravel(keywordHandler, destination, cost, position, condition)
     local keywords = {"bring me to " .. destination:lower()}
-    keywordHandler:addKeyword(keywords, function(player, message, keywords, parameters, node)
-        if player:getBalance() < cost then
-            npcHandler:say("I'm sorry, but you don't have enough money to pay for the trip.", npc, player)
+    keywordHandler:addKeyword(keywords, function(npc, player, message, keywords, parameters, node)
+        if player:isPlayer() then
+            if condition and not condition(player) then
+                npcHandler:say("I'm sorry, but I can't take you to " .. destination .. " at the moment.", npc, player)
+                return true
+            end
+            if player:getBankBalance() < cost then
+                npcHandler:say("I'm sorry, but you don't have enough money to pay for the trip.", npc, player)
+                return true
+            end
+            player:removeMoneyBank(cost)
+            player:teleportTo(position)
+            player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+            npcHandler:say("Have a nice trip!", npc, player)
             return true
         end
-        player:removeMoneyBank(cost)
-        player:teleportTo(position)
-        player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-        npcHandler:say("Have a nice trip!", npc, player)
-        return true
     end, {npcHandler = npcHandler})
 end
 
--- Add instant travel options
+local function addTravelKeyword(keyword, cost, destination, action, condition)
+    local keywords = {"bring me to " .. keyword:lower()}
+    keywordHandler:addKeyword(keywords, StdModule.say, { npcHandler = npcHandler, text = "I'm sorry but I don't sail there." }, condition)
+    local travelKeyword = keywordHandler:addKeyword(keywords, StdModule.say, { npcHandler = npcHandler, text = "Do you seek a passage to " .. keyword:titleCase() .. " for |TRAVELCOST|?", cost = cost, discount = "postman" })
+    travelKeyword:addChildKeyword({"yes"}, StdModule.travel, { npcHandler = npcHandler, premium = false, cost = cost, discount = "postman", destination = destination }, nil, action)
+    travelKeyword:addChildKeyword({"no"}, StdModule.say, { npcHandler = npcHandler, text = "We would like to serve you some time.", reset = true })
+end
+
+-- Add travel options
 addInstantTravel(keywordHandler, "Ab'Dendriel", 130, Position(32734, 31668, 6))
 addInstantTravel(keywordHandler, "Edron", 160, Position(33175, 31764, 6))
 addInstantTravel(keywordHandler, "Venore", 170, Position(32954, 32022, 6))
@@ -82,39 +90,17 @@ addInstantTravel(keywordHandler, "Oramond", 150, Position(33479, 31985, 7))
 addInstantTravel(keywordHandler, "Krailos", 230, Position(33492, 31712, 6))
 
 -- Special case for Yalahar with condition
-keywordHandler:addKeyword({"bring me to yalahar"}, function(player, message, keywords, parameters, node)
-    if player:getStorageValue(Storage.SearoutesAroundYalahar.Thais) == 1 or player:getStorageValue(Storage.SearoutesAroundYalahar.TownsCounter) >= 5 then
-        npcHandler:say("I'm sorry, but I can't take you to Yalahar at the moment.", npc, player)
-        return true
-    end
-    if player:getBalance() < 200 then
-        npcHandler:say("I'm sorry, but you don't have enough money to pay for the trip.", npc, player)
-        return true
-    end
-    player:removeMoneyBank(200)
-    player:teleportTo(Position(32816, 31272, 6))
-    player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-    npcHandler:say("Have a nice trip!", npc, player)
-    return true
-end, {npcHandler = npcHandler})
-
--- Travel
-local function addTravelKeyword(keyword, cost, destination, action, condition)
-	if condition then
-		keywordHandler:addKeyword({ keyword }, StdModule.say, { npcHandler = npcHandler, text = "I'm sorry but I don't sail there." }, condition)
-	end
-
-	local travelKeyword = keywordHandler:addKeyword({ keyword }, StdModule.say, { npcHandler = npcHandler, text = "Do you seek a passage to " .. keyword:titleCase() .. " for |TRAVELCOST|?", cost = cost, discount = "postman" })
-	travelKeyword:addChildKeyword({ "yes" }, StdModule.travel, { npcHandler = npcHandler, premium = false, cost = cost, discount = "postman", destination = destination }, nil, action)
-	travelKeyword:addChildKeyword({ "no" }, StdModule.say, { npcHandler = npcHandler, text = "We would like to serve you some time.", reset = true })
+local function canTravelToYalahar(player)
+    return player:getStorageValue(Storage.SearoutesAroundYalahar.Thais) ~= 1 and player:getStorageValue(Storage.SearoutesAroundYalahar.TownsCounter) < 5
 end
+addInstantTravel(keywordHandler, "Yalahar", 200, Position(32816, 31272, 6), canTravelToYalahar)
 
+-- Add travel keywords
 addTravelKeyword("carlin", 110, Position(32387, 31820, 6), function(player)
-	if player:getStorageValue(Storage.Postman.Mission01) == 1 then
-		player:setStorageValue(Storage.Postman.Mission01, 2)
-	end
+    if player:getStorageValue(Storage.Postman.Mission01) == 1 then
+        player:setStorageValue(Storage.Postman.Mission01, 2)
+    end
 end)
-
 addTravelKeyword("ab'dendriel", 130, Position(32734, 31668, 6))
 addTravelKeyword("edron", 160, Position(33175, 31764, 6))
 addTravelKeyword("venore", 170, Position(32954, 32022, 6))
@@ -122,16 +108,14 @@ addTravelKeyword("port hope", 160, Position(32527, 32784, 6))
 addTravelKeyword("roshamuul", 210, Position(33494, 32567, 7))
 addTravelKeyword("svargrond", 180, Position(32341, 31108, 6))
 addTravelKeyword("liberty bay", 180, Position(32285, 32892, 6))
-addTravelKeyword("yalahar", 200, Position(32816, 31272, 6), nil, function(player)
-	return player:getStorageValue(Storage.SearoutesAroundYalahar.Thais) ~= 1 and player:getStorageValue(Storage.SearoutesAroundYalahar.TownsCounter) < 5
-end)
+addTravelKeyword("yalahar", 200, Position(32816, 31272, 6), nil, canTravelToYalahar)
 addTravelKeyword("oramond", 150, Position(33479, 31985, 7))
 addTravelKeyword("krailos", 230, Position(33492, 31712, 6))
 
 -- Kick
 keywordHandler:addKeyword({ "kick" }, StdModule.kick, { npcHandler = npcHandler, destination = { Position(32320, 32219, 6), Position(32321, 32210, 6) } })
 
--- Basic
+-- Basic Dialogue
 keywordHandler:addKeyword({ "name" }, StdModule.say, { npcHandler = npcHandler, text = "My name is Captain Bluebear from the Royal Tibia Line." })
 keywordHandler:addKeyword({ "job" }, StdModule.say, { npcHandler = npcHandler, text = "I am the captain of this sailing-ship." })
 keywordHandler:addKeyword({ "captain" }, StdModule.say, { npcHandler = npcHandler, text = "I am the captain of this sailing-ship." })
@@ -157,11 +141,10 @@ keywordHandler:addKeyword({ "darama" }, StdModule.say, { npcHandler = npcHandler
 keywordHandler:addKeyword({ "ghost" }, StdModule.say, { npcHandler = npcHandler, text = "Many people who sailed to Darashia never returned because they were attacked by a ghostship! I'll never sail there!" })
 keywordHandler:addKeyword({ "thais" }, StdModule.say, { npcHandler = npcHandler, text = "This is Thais. Where do you want to go?" })
 
+-- NPC Messages
 npcHandler:setMessage(MESSAGE_GREET, "Welcome on board, |PLAYERNAME|. Where can I {sail} you today?")
 npcHandler:setMessage(MESSAGE_FAREWELL, "Good bye. Recommend us if you were satisfied with our service.")
 npcHandler:setMessage(MESSAGE_WALKAWAY, "Good bye then.")
 
-npcHandler:addModule(FocusModule:new(), npcConfig.name, true, true, true)
-
--- npcType registering the npcConfig table
+-- Add the NPC to the game
 npcType:register(npcConfig)
